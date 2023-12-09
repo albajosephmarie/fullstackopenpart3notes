8dfa96ba6c9d319f25014d2e48817940215e2865
albajosephmarie <albajosephmarie@gmail.com>
2023-12-10 06:10:50 +0800

    uibuild

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const unknownEndpont = (request, response) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-app.use(express.static('build'))
+app.use(express.static('dist'))
 
 let notes = [
   {
